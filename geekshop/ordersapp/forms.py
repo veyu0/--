ee63a2c4aac4a7c6cc9f14c90066779261ac98b0
Vera Koberman <50861303+KOberman7@users.noreply.github.com>
2021-04-23 17:27:51 +0500

    Update forms.py

--- geekshop/ordersapp/forms.py
+++ geekshop/ordersapp/forms.py
@@ -3,25 +3,24 @@ from ordersapp.models import Order, OrderItem
 
 
 class OrderForm(forms.ModelForm):
-   class Meta:
-       model = Order
-       exclude = ('user',)
+    class Meta:
+        model = Order
+        exclude = ('user',)
 
-   def __init__(self, *args, **kwargs):
-       super(OrderForm, self).__init__(*args, **kwargs)
-       for field_name, field in self.fields.items():
-           field.widget.attrs['class'] = 'form-control'
+    def __init__(self, *args, **kwargs):
+        super(OrderForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
 
 
 class OrderItemForm(forms.ModelForm):
-
     price = forms.CharField(label='цена', required=False)
 
-   class Meta:
-       model = OrderItem
-       exclude = ()
+    class Meta:
+        model = OrderItem
+        exclude = ()
 
-   def __init__(self, *args, **kwargs):
-       super(OrderItemForm, self).__init__(*args, **kwargs)
-       for field_name, field in self.fields.items():
-           field.widget.attrs['class'] = 'form-control'
+    def __init__(self, *args, **kwargs):
+        super(OrderItemForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
