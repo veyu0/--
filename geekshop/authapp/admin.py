5a8752711dafc842f3beb1bb0d737042df07d97c
Vera K <50861303+veyu0@users.noreply.github.com>
2021-06-29 21:14:34 +0500

    Delete geekshop directory

--- geekshop/authapp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from authapp.models import ShopUser
-
-admin.site.register(ShopUser)
--- geekshop/ordersapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class OrdersappConfig(AppConfig):
-    name = 'ordersapp'
